6027a7a29cccb20d2ac39601b040c3c8d908f1e7
Adrian Murawski <adrianmurawski79@gmail.com>
2024-12-15 17:42:31 +0100

    added temporary dashboard

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -1,59 +1,24 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import DashboardHeader from './modules/DashboardHeader.js';
+import CommercialSidebar from './modules/CommercialSidebar.js';
+import './Dashboard.css'
 
-const Dashboard = () => {
-	const navigate = useNavigate();
-
-	const Logout = () => navigate('/');
-	const goToStatistics = () => navigate('/statistics');
-	const goToUsers = () => navigate('/users');
-	const goToRegisterUser = () => navigate('/register_user');
-	const goToEquipment = () => navigate('/equipment');
-	const goToInUse = () => navigate('/in_use');
-	const goToCart = () => navigate('/cart');
-	const goToContracts = () => navigate('/contracts');
-	const goToRecipes = () => navigate('/recipes');
-	const goToCommercialBreweries = () => navigate('/commercial_breweries');
-
-	const userType = localStorage.getItem('userType');
-
-	const handleLogout = () => {
-		localStorage.removeItem('userType');
-		Logout();
-	};
+const CommercialDashboard = () => {
+	// const navigate = useNavigate();
 
 	return (
-    	<div className="dashboard" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px' }}>
-			<button onClick={handleLogout} style={{ alignSelf: 'flex-end'}}>Wyloguj</button>
-			<h1>Witaj w ekranie głównym!</h1>
-				<p>Opis</p>
-				{userType === 'admin' && (
-					<div style={{ padding: '10px' }}>
-						<button onClick={goToUsers}>Użytkownicy</button>
-						<button onClick={goToStatistics}>Statystyki</button>
-					</div>
-				)}
-				{userType === 'commercial' && (
-					<div style={{ padding: '10px' }}>
-						<button onClick={goToRegisterUser}>Zarejestruj użytkownika</button>
-						<button onClick={goToEquipment}>Urządzenia</button>
-						<button onClick={goToContracts}>Zlecenia</button>
-						<button onClick={goToInUse}>W użyciu</button>
-						<button onClick={goToStatistics}>Statystyki</button>
-					</div>
-				)}
-				{userType === 'contract' && (
-					<div style={{ padding: '10px' }}>
-						<button onClick={goToRegisterUser}>Zarejestruj użytkownika</button>
-						<button onClick={goToCommercialBreweries}>Browary Komercyjne</button>
-						<button onClick={goToCart}>Koszyk</button>
-						<button onClick={goToContracts}>Zlecenia</button>
-						<button onClick={goToRecipes}>Przepisy</button>
-						<button onClick={goToStatistics}>Statystyki</button>
-					</div>
-				)}
+        <div>
+            <DashboardHeader />
+			<div className='app-container'>
+				<CommercialSidebar />
+				<div className="content">
+					<h1>Witaj w aplikacji!</h1>
+					<p>To jest przykładowy tekst obok panelu.</p>
+				</div>
+			</div>
 		</div>
-	);
+    );
 };
 
-export default Dashboard;
+export default CommercialDashboard;
