eb3e904a1ace319554810de6e98bb756295a653a
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2024-12-16 20:30:49 +0100

    basic api configuration in react, check-username-unique usage

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/checkusernameunique/", views.CheckUsernameUniqueView.as_view(), name="check_username_unique"),
+    path("api/check-username-unique/", views.CheckUsernameUniqueView.as_view(), name="check_username_unique"),
     path("api/register/commercial/", RegisterCommercialView.as_view(), name="register_commercial"),
     path("api/register/contract/", views.RegisterContractBreweryView.as_view(), name="register_contract"),
     path("api/login/", LoginView.as_view(), name="login"),
