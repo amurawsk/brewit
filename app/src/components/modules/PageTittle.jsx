c8c0cba7997a7329d15f4a89743e7f45f9a0dfdc
Adrian Murawski <adrianmurawski79@gmail.com>
2024-12-20 19:39:54 +0100

    added page tittle module

---
+++ app/src/components/modules/PageTittle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './PageTittle.module.css';
+
+
+const PageTittle = () => {
+    return (
+        <h1 className={styles.pageTittle}>Dodaj urządzenie</h1>
+    )
+}
+export default PageTittle
