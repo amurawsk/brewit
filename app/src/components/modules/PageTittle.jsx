0092fdd6ca944958754ab11ff9fcefccedf99b4f
Adrian Murawski <adrianmurawski79@gmail.com>
2024-12-20 19:40:39 +0100

    argument added, prettier changes

--- app/src/components/modules/PageTittle.jsx
+++ app/src/components/modules/PageTittle.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 import styles from './PageTittle.module.css';
 
-
-const PageTittle = () => {
-    return (
-        <h1 className={styles.pageTittle}>Dodaj urządzenie</h1>
-    )
-}
-export default PageTittle
+const PageTittle = (text) => {
+    return <h1 className={styles.pageTittle}>{text}</h1>;
+};
+export default PageTittle;
