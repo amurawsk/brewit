f5964c7a4868a288b3dc13ee4cf200567821f3fd
Adrian Murawski <adrianmurawski79@gmail.com>
2024-12-20 18:14:28 +0100

    extracted add device form from add device

--- app/src/components/pages/commercial/AddDevice.js
+++ app/src/components/pages/commercial/AddDevice.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styles from './AddDevice.module.css'
 import DashboardHeader from '../../modules/DashboardHeader.js';
 import CommercialSidebar from '../../modules/commercial/CommercialSidebar.js';
+import AddDeviceForm from '../../modules/commercial/AddDeviceForm.jsx'
 import api from '../../../api.js';
 
 const AddDevice = () => {
@@ -24,18 +25,18 @@ const AddDevice = () => {
 	const postData = async () => {
 		try {
 			const response = await api.post(`devices/add/`, {
-				name: name,
-				device_type: device_type,
-				serial_number: serial_number,
-				capacity: parseFloat(capacity),
-				temperature_min: parseFloat(temperature_min),
-				temperature_max: parseFloat(temperature_max),
-				sour_beers: sour_beers, 
-				carbonation: carbonation,
-				supported_containers: supported_containers,
+				"name": name,
+				"device_type": device_type,
+				"serial_number": serial_number,
+				"capacity": parseFloat(capacity),
+				"temperature_min": parseFloat(temperature_min),
+				"temperature_max": parseFloat(temperature_max),
+				"sour_beers": sour_beers, 
+				"carbonation": carbonation,
+				"supported_containers": supported_containers,
 			});
+			// devices();
 			if (response.status === 201) {
-				devices();
 			} else {
 				console.log(response);
 				// TODO: Handle error
@@ -52,79 +53,21 @@ const AddDevice = () => {
 				<CommercialSidebar />
 				<div className={styles.addEquipment}>
 					<h1 className={styles.addDeviceTittle}>Dodaj urządzenie</h1>
-					<div className={styles.deviceTypeDropbox}>
-						<label className={styles.addEquipmentLabel} htmlFor="device_type"><b>Wybierz typ urządzenia: </b></label>
-						<select className={styles.dropboxInput} value={device_type} onChange={(e) => {console.log(e.target.value); setDeviceType(e.target.value)}}>
-							<option value="" disabled>Wybierz urządzenie</option>
-							<option value="BT">Tank Warzelny</option>
-							<option value="FT">Pojemnik fermentacyjny</option>
-							<option value="AC">Kocioł do leżakowania</option>
-							<option value="BE">Urządzenie do rozlewania</option>
-						</select>
-					</div>
-					<form className={styles.addDeviceForm} onSubmit={postData}>
-						{device_type !== "" && (
-							<div>
-								<div>
-									<label className={styles.addEquipmentLabel} htmlFor="name"><b>Nazwa urządzenia: </b></label>
-									<input className={styles.addEquipmentInput} type="text" placeholder="Wpisz nazwę urządzenia" onChange={(e) => setName(e.target.value)} name="name" required />
-								</div>
-								<div>
-									<label className={styles.addEquipmentLabel} htmlFor="serial_number"><b>Numer seryjny: </b></label>
-									<input className={styles.addEquipmentInput} type="text" placeholder="Wpisz numer seryjny" onChange={(e) => setSerialNumber(e.target.value)} name="serial_number" required />
-								</div>
-								<div>
-									<label className={styles.addEquipmentLabel} htmlFor="capacity"><b>Pojemność: </b></label>
-									<input className={styles.addEquipmentInput} type="text" placeholder="Wpisz pojemność" onChange={(e) => setCapacity(e.target.value)} name="capacity" required />
-								</div>
-							</div> 
-							
-						)}
-						{device_type !== 'BE' && device_type !== "" && (
-							<div>
-								<div>
-									<label className={styles.addEquipmentLabel} htmlFor="temperature_min"><b>Temperatura minimalna: </b></label>
-									<input className={styles.addEquipmentInput} type="text" placeholder="Wpisz temperaturę minimlalną" onChange={(e) => setTemperatureMin(e.target.value)} name="temperature_min" required />
-								</div>
-								<div>
-									<label className={styles.addEquipmentLabel} htmlFor="temperature_max"><b>Temperatura maksymalna: </b></label>
-									<input className={styles.addEquipmentInput} type="text" placeholder="Wpisz temperaturę maksymalną" onChange={(e) => setTemperatureMax(e.target.value)} name="temperature_max" required />
-								</div>
-							</div>
-							
-						)}
-						{device_type !== "" && (
-							<div className={styles.sourBeerCheckbox}>
-								<label className={styles.addEquipmentLabel} htmlFor="sour_beers"><b>Do produkcji kwaśnych piw: </b></label>
-								<input className={styles.addEquipmentInputCheckbox} type="checkbox" onChange={(e) => setSourBeers(e.target.checked)}></input>
-							</div>
-						)}
-						{(device_type === 'BT' || device_type === 'BE') && (
-							<div>
-								<label className={styles.addEquipmentLabel} htmlFor="carbonation"><b>Nagazowanie: </b></label>
-								<select className={styles.dropboxInput} value={carbonation} onChange={(e) => setCarbonation(e.target.value)}>
-									<option value="CO2">CO2</option>
-									<option value="NO2">NO2</option>
-									<option value="mieszanka CO2/NO2">Mieszanka CO2/NO2</option>
-									<option value="nie">Nie</option>
-								</select>
-							</div>
-						)}
-						{(device_type === 'BE') && (
-							<div>
-								<label className={styles.addEquipmentLabel} htmlFor="supported_containers"><b>Obsługiwane pojemniki: </b></label>
-								<select className={styles.dropboxInput} value={supported_containers} onChange={(e) => setSupportedContainers(e.target.value)}>
-									<option value="butelka">Butelki</option>
-									<option value="puszka">Puszki</option>
-									<option value="keg">Kegi</option>
-								</select>
-							</div>
-						)}
-						<button className={styles.insertDeviceButton} type="submit">Dodaj urządzenie</button>
-					</form>
+					<AddDeviceForm 
+						device_type = {device_type}
+						setDeviceType = {setDeviceType}
+						setName = {setName}
+						setSerialNumber = {setSerialNumber}
+						setCapacity = {setCapacity}
+						setTemperatureMin = {setTemperatureMin}
+						setTemperatureMax = {setTemperatureMax}
+						setSourBeers = {setSourBeers}
+						setCarbonation = {setCarbonation}
+						setSupportedContainers = {setSupportedContainers}
+						postData = {postData}
+					/>
 				</div>
 			</div>
-			
 		</div>
 
     );
