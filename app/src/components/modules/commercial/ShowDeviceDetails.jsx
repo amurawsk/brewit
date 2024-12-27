1eaae91d21b7337168b9398412f49922ea9436dc
Adrian Murawski <adrianmurawski79@gmail.com>
2024-12-27 14:10:18 +0100

    added correct device details panel, closes #21, #22

--- app/src/components/modules/commercial/ShowDeviceDetails.jsx
+++ app/src/components/modules/commercial/ShowDeviceDetails.jsx
@@ -59,6 +59,7 @@ const ShowDeviceDetails = ({
                     <input
                         className={styles.panelContentInput}
                         type="text"
+                        placeholder='Wpisz nazwę'
                         value={deviceFields.name}
                         onChange={(e) =>
                             handleFieldChange('name', e.target.value)
@@ -68,7 +69,7 @@ const ShowDeviceDetails = ({
                     <input
                         className={styles.panelContentInput}
                         type="text"
-                        value={deviceFields.type}
+                        value={deviceFields.device_type}
                         readOnly
                     />
                     <label className={styles.panelContentLabel}>
@@ -79,22 +80,43 @@ const ShowDeviceDetails = ({
                         type="number"
                         min="0"
                         step="0.1"
+                        placeholder='Wpisz pojemność'
                         value={deviceFields.capacity}
                         onChange={(e) =>
                             handleFieldChange('capacity', e.target.value)
                         }
                     />
-                    <label className={styles.panelContentLabel}>
-                        Zakres temperatur:
-                    </label>
-                    <input
-                        className={styles.panelContentInput}
-                        type="text"
-                        value={deviceFields.temperature_min}
-                        onChange={(e) =>
-                            handleFieldChange('temperature_min', e.target.value)
-                        }
-                    />
+                    {deviceFields.device_type !== 'BE' && deviceFields.device_type && (
+                        <div className={styles.panelContent}>
+                            <label className={styles.panelContentLabel}>
+                                Temperatura minimalna (℃):
+                            </label>
+                            <input
+                                className={styles.panelContentInput}
+                                type="number"
+                                step='0.5'
+                                placeholder='Wpisz temperaturę minimalną'
+                                value={deviceFields.temperature_min}
+                                onChange={(e) =>
+                                    handleFieldChange('temperature_min', e.target.value)
+                                }
+                            />
+                            <label className={styles.panelContentLabel}>
+                                Temperatura maksymalna (℃):
+                            </label>
+                            <input
+                                className={styles.panelContentInput}
+                                type="number"
+                                step='0.5'
+                                placeholder='Wpisz temperaturę maksymalną'
+                                value={deviceFields.temperature_max}
+                                onChange={(e) =>
+                                    handleFieldChange('temperature_max', e.target.value)
+                                }
+                            />
+                        </div>
+                        
+                    )}
                     <label className={styles.panelContentLabel}>
                         Kwaśne piwa:
                     </label>
@@ -107,102 +129,99 @@ const ShowDeviceDetails = ({
                             handleFieldChange('sour_beers', e.target.checked)
                         }
                     />
-                    <label className={styles.panelContentLabel}>
-                        Karbonizacja:
-                    </label>
-                    <div>
-                        <input
-                            className={styles.checkboxInput}
-                            type="checkbox"
-                            id="CO2"
-                            name="carbonation"
-                            value="CO2"
-                            checked={deviceFields.carbonation.includes('CO2')}
-                            onChange={(e) =>
-                                handleFieldChange(
-                                    'carbonation',
-                                    e.target.value,
-                                    e.target.checked
-                                )
-                            }
-                        />
-                        <label
-                            className={styles.panelContentLabel}
-                            htmlFor="CO2">
-                            CO2
-                        </label>
+                    {(deviceFields.device_type === 'BT' || deviceFields.device_type === 'BE') && (
+                        <div className={styles.panelContent}>
+                            <label className={styles.panelContentLabel}>
+                                Nagazowanie:
+                            </label>
+                            <div>
+                                <input
+                                    className={styles.checkboxInput}
+                                    type="checkbox"
+                                    id="CO2"
+                                    name="carbonation"
+                                    value="CO2"
+                                    checked={deviceFields.carbonation.includes('CO2')}
+                                    onChange={(e) =>
+                                        handleFieldChange(
+                                            'carbonation',
+                                            e.target.value,
+                                            e.target.checked
+                                        )
+                                    }
+                                />
+                                <label
+                                    className={styles.checkboxInputLabel}
+                                    htmlFor="CO2">
+                                    CO2
+                                </label>
 
-                        <input
-                            className={styles.checkboxInput}
-                            type="checkbox"
-                            id="N2"
-                            name="carbonation"
-                            value="N2"
-                            checked={deviceFields.carbonation.includes('N2')}
-                            onChange={(e) =>
-                                handleFieldChange(
-                                    'carbonation',
-                                    e.target.value,
-                                    e.target.checked
-                                )
-                            }
-                        />
-                        <label
-                            className={styles.panelContentLabel}
-                            htmlFor="N2">
-                            N2
-                        </label>
+                                <input
+                                    className={styles.checkboxInput}
+                                    type="checkbox"
+                                    id="N2"
+                                    name="carbonation"
+                                    value="N2"
+                                    checked={deviceFields.carbonation.includes('N2')}
+                                    onChange={(e) =>
+                                        handleFieldChange(
+                                            'carbonation',
+                                            e.target.value,
+                                            e.target.checked
+                                        )
+                                    }
+                                />
+                                <label
+                                    className={styles.checkboxInputLabel}
+                                    htmlFor="N2">
+                                    N2
+                                </label>
 
-                        <input
-                            className={styles.checkboxInput}
-                            type="checkbox"
-                            id="CO2-N2"
-                            name="carbonation"
-                            value="mieszanka"
-                            checked={deviceFields.carbonation.includes(
-                                'mieszanka'
-                            )}
-                            onChange={(e) =>
-                                handleFieldChange(
-                                    'carbonation',
-                                    e.target.value,
-                                    e.target.checked
-                                )
-                            }
-                        />
-                        <label
-                            className={styles.panelContentLabel}
-                            htmlFor="CO2-N2">
-                            mieszanka
-                        </label>
-                    </div>
-                    <label className={styles.panelContentLabel}>
-                        Obsługiwane pojemniki:
-                    </label>
-                    <input
-                        className={styles.panelContentInput}
-                        type="text"
-                        value={deviceFields.supported_containers}
-                        onChange={(e) =>
-                            handleFieldChange(
-                                'supported_containers',
-                                e.target.value
-                            )
-                        }
-                    />
-                    <label className={styles.panelContentLabel}>
-                        Cena (zł):
-                    </label>
-                    <input
-                        className={styles.panelContentInput}
-                        type="number"
-                        min="0"
-                        step="0.1"
-                        value={deviceFields.price}
-                        onChange={(e) =>
-                            handleFieldChange('price', e.target.value)
-                        }
-                    />
+                                <input
+                                    className={styles.checkboxInput}
+                                    type="checkbox"
+                                    id="CO2-N2"
+                                    name="carbonation"
+                                    value="mieszanka"
+                                    checked={deviceFields.carbonation.includes(
+                                        'mieszanka'
+                                    )}
+                                    onChange={(e) =>
+                                        handleFieldChange(
+                                            'carbonation',
+                                            e.target.value,
+                                            e.target.checked
+                                        )
+                                    }
+                                />
+                                <label
+                                    className={styles.checkboxInputLabel}
+                                    htmlFor="CO2-N2">
+                                    mieszanka
+                                </label>
+                            </div>
+                        </div>
+                    )}
+
+                    {deviceFields.device_type === 'BE' && ( 
+                        <div className={styles.panelContent}>
+                            <label className={styles.panelContentLabel}>
+                                Obsługiwane pojemniki:
+                            </label>
+                            <input
+                                className={styles.panelContentInput}
+                                type="text"
+                                placeholder='Np. butelki, puszki, kegi...'
+                                value={deviceFields.supported_containers}
+                                onChange={(e) =>
+                                    handleFieldChange(
+                                        'supported_containers',
+                                        e.target.value
+                                    )
+                                }
+                            />
+                        </div>
+                    )}
 
                     <div className={styles.panelButtons}>
                         <button
