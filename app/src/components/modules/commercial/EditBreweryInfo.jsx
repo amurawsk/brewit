a9c5ded621449e317b9dd61e28c9f7ecb0716b82
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-04 22:49:39 +0100

    added edit brewery info, closes #44

---
+++ app/src/components/modules/commercial/EditBreweryInfo.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import styles from './EditBreweryInfo.module.css';
+
+const EditBreweryInfo = ({ isPanelOpen, setIsPanelOpen, breweryData }) => {
+    const [currentBreweryData, setCurrentBreweryData] = useState(breweryData);
+
+    const closePanel = () => {
+        setIsPanelOpen(false);
+        setCurrentBreweryData(breweryData);
+    };
+
+    const editData = () => {
+        console.log(currentBreweryData);
+        setIsPanelOpen(false);
+        setCurrentBreweryData(breweryData);
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCurrentBreweryData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div>
+            {isPanelOpen && (
+                <div className={styles.overlay}>
+                    <div className={styles.panel}>
+                        <h2 className={styles.header}>Edytuj informacje</h2>
+                        <form className={styles.editDataForm}>
+                            <div className={styles.leftColumn}>
+                                <div>
+                                    <label className={styles.dataLabel}>Nazwa browaru</label>
+                                    <input
+                                        className={styles.dataInput}
+                                        type="text"
+                                        placeholder="Podaj nazwę"
+                                        name="name"
+                                        value={currentBreweryData.name}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className={styles.dataLabel}>Email kontaktowy</label>
+                                    <input
+                                        className={styles.dataInput}
+                                        type="email"
+                                        placeholder="Podaj email"
+                                        name="email"
+                                        value={currentBreweryData.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className={styles.dataLabel}>Opis (opcjonalne)</label>
+                                    <textarea
+                                        className={`${styles.dataInput} ${styles.descriptionInput}`}
+                                        placeholder="Podaj opis swojego browaru"
+                                        name="description"
+                                        value={currentBreweryData.description}
+                                        onChange={handleChange}
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <div className={styles.rightColumn}>
+                                <div>
+                                    <label className={styles.dataLabel}>Telefon kontaktowy</label>
+                                    <input
+                                        className={styles.dataInput}
+                                        type="text"
+                                        placeholder="Podaj numer"
+                                        name="phone_number"
+                                        value={currentBreweryData.phone_number}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className={styles.dataLabel}>NIP</label>
+                                    <input
+                                        className={styles.dataInput}
+                                        type="text"
+                                        placeholder="Podaj NIP"
+                                        name="nip"
+                                        value={currentBreweryData.nip}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label className={styles.dataLabel}>Adres</label>
+                                    <input
+                                        className={styles.dataInput}
+                                        type="text"
+                                        placeholder="Podaj adres"
+                                        name="address"
+                                        value={currentBreweryData.address}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                        <div className={styles.buttonsContainer}>
+                            <button
+                                className={styles.backButton}
+                                onClick={closePanel}
+                            >
+                                Zamknij
+                            </button>
+                            <button
+                                className={styles.changeDataButton}
+                                onClick={editData}
+                            >
+                                Zmień dane
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default EditBreweryInfo;
--- app/src/components/pages/commercial/CommercialBrewery.jsx
+++ app/src/components/pages/commercial/CommercialBrewery.jsx
@@ -1,43 +1,59 @@
-import React from 'react';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState, useEffect } from 'react';
 
 import DashboardHeader from '../../modules/DashboardHeader.jsx';
 import CommercialSidebar from '../../modules/commercial/CommercialSidebar.jsx';
 import BreweryInfo from '../../modules/commercial/BreweryInfo.jsx';
 import PageTittleWithButton from '../../utils/PageTittleWithButton.jsx';
 import PageTittle from '../../utils/PageTittle.jsx';
-
-import styles from './CommercialBrewery.module.css';
+import EditBreweryInfo from '../../modules/commercial/EditBreweryInfo';
 import BreweryShortStats from '../../modules/commercial/BreweryShortStats.jsx';
 
+import styles from './CommercialBrewery.module.css';
+
 const CommercialBrewery = () => {
-    // TODO
-    const editInfo = () => {};
-
+    const [isPanelOpen, setIsPanelOpen] = useState(false);
+    const [breweryData, setBreweryData] = useState(null);
+    const [statsData, setStatsData] = useState(null);
+    
     // TODO mock
-    const brewery_info = {
-        name: 'Przykład',
-        email: 'example@gmail.com',
-        phone_number: '123456789',
-        nip: '12345678901',
-        address: 'Szkolna 1',
-        description:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate mollitia velit corrupti harum possimus ipsam nisi odit explicabo fugiat ea, tempora facilis accusantium perferendis voluptas minima, nam ratione numquam aliquid.',
-    };
-    const stats_info = {
-        no_devices: 1,
-        no_bt: 1,
-        no_ft: 0,
-        no_ac: 0,
-        no_be: 0,
-        no_orders: 3,
-        no_new: 1,
-        no_current: 1,
-        no_past: 1,
-        no_rejected: 0,
-        no_employees: 1,
-    };
+    const getData = () => {
+        const brewery_info = {
+            name: 'Przykład',
+            email: 'example@gmail.com',
+            phone_number: '123456789',
+            nip: '12345678901',
+            address: 'Szkolna 1',
+            description:
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate mollitia velit corrupti harum possimus ipsam nisi odit explicabo fugiat ea, tempora facilis accusantium perferendis voluptas minima, nam ratione numquam aliquid.',
+        };
+        const stats_info = {
+            no_devices: 1,
+            no_bt: 1,
+            no_ft: 0,
+            no_ac: 0,
+            no_be: 0,
+            no_orders: 3,
+            no_new: 1,
+            no_current: 1,
+            no_past: 1,
+            no_rejected: 0,
+            no_employees: 1,
+        };
 
+        setBreweryData(brewery_info);
+        setStatsData(stats_info);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+    
+
+    // TODO
+    const editInfo = () => {
+        setIsPanelOpen(true);
+    };
+    
     return (
         <div>
             <DashboardHeader />
@@ -49,9 +65,16 @@ const CommercialBrewery = () => {
                         buttonText="Edytuj informacje"
                         buttonFunction={editInfo}
                     />
-                    <BreweryInfo breweryData={brewery_info} />
+                    {breweryData !== null && (
+                        <BreweryInfo breweryData={breweryData} />
+                    )}
+                    {isPanelOpen && breweryData !== null && (
+                        <EditBreweryInfo isPanelOpen={isPanelOpen} setIsPanelOpen={setIsPanelOpen} breweryData={breweryData} />
+                    )}
                     <PageTittle text="Statystyki" />
-                    <BreweryShortStats statsData={stats_info} />
+                    {statsData !== null && (
+                        <BreweryShortStats statsData={statsData} />
+                    )}
                 </div>
             </div>
         </div>
