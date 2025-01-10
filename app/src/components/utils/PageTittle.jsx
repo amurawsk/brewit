a6c1d3c1c4463d8cf52bd77055756f143f13367c
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-10 13:13:11 +0100

    removed backend ci, added comments

--- app/src/components/utils/PageTittle.jsx
+++ app/src/components/utils/PageTittle.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import styles from './PageTittle.module.css';
 
+/**
+ * Component created to use equal page tittle in all pages
+ * @param text - text which will be the header of the page 
+ */
 const PageTittle = ({ text }) => {
     return (
         <div>
--- app/src/components/utils/PageTittleWithButton.jsx
+++ app/src/components/utils/PageTittleWithButton.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import styles from './PageTittleWithButton.module.css';
 
+/**
+ * Similarly to PageTittle, but has a button on the right side
+ * @param text - text which will be the header of the page 
+ * @param buttonText - text which will be displayed on button 
+ * @param buttonFunction - function executed when button is clicked
+ */
 const PageTittleWithButton = ({ text, buttonText, buttonFunction }) => {
     return (
         <div className={styles.container}>
