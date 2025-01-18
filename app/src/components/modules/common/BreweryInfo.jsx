7d5ee07d5853e764530e7f235f807f94ad8ee9e6
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-18 23:03:22 +0100

    changed info

--- app/src/components/modules/common/BreweryInfo.jsx
+++ app/src/components/modules/common/BreweryInfo.jsx
@@ -1,49 +1,54 @@
 import React from 'react';
-
+import { FaEnvelope, FaPhone, FaMapMarkerAlt, FaIdCard, FaUserTie } from 'react-icons/fa';
 import styles from './BreweryInfo.module.css';
 
 /**
- * BreweryInfo - shows brewery details - name, email, phone_number, ceo / nip
+ * BreweryInfo - shows brewery details - name, email, phone_number, ceo / nip, no_employees
  * @param breweryData - data
  */
 const BreweryInfo = ({ breweryData }) => {
     return (
         <div className={styles.content}>
             <div className={styles.section}>
-                <h3>Informacje</h3>
-                <p>
-                    Nazwa browaru: <b>{breweryData.name}</b>
-                </p>
-                <p>
-                    Email kontaktowy: <b>{breweryData.email}</b>
-                </p>
-                <p>
-                    Telefon kontaktowy: <b>{breweryData.phone_number}</b>
-                </p>
+                <h3 className={styles.breweryName}>Browar - {breweryData.name}</h3>
+                <div className={`${styles.infoCard} ${styles.email}`}>
+                    <FaEnvelope className={styles.icon} />
+                    <span>Email kontaktowy:</span> <b>{breweryData.email}</b>
+                </div>
+                <div className={`${styles.infoCard} ${styles.phone}`}>
+                    <FaPhone className={styles.icon} />
+                    <span>Telefon kontaktowy:</span> <b>{breweryData.phone_number}</b>
+                </div>
                 {localStorage.getItem('userType') === 'commercial_brewery' && (
                     <>
-                        <p>
-                            NIP: <b>{breweryData.nip}</b>
-                        </p>
-                        <p>
-                            Adres: <b>{breweryData.address}</b>
-                        </p>
+                        <div className={`${styles.infoCard} ${styles.nip}`}>
+                            <FaIdCard className={styles.icon} />
+                            <span>NIP:</span> <b>{breweryData.nip}</b>
+                        </div>
+                        <div className={`${styles.infoCard} ${styles.address}`}>
+                            <FaMapMarkerAlt className={styles.icon} />
+                            <span>Adres:</span> <b>{breweryData.address}</b>
+                        </div>
                     </>
                 )}
                 {localStorage.getItem('userType') === 'contract_brewery' && (
-                    <>
-                        <p>
-                            Imię i nazwisko właściciela:{' '}
-                            <b>{breweryData.ceo}</b>
-                        </p>
-                    </>
+                    <div className={`${styles.infoCard} ${styles.ceo}`}>
+                        <FaUserTie className={styles.icon} />
+                        <span>Imię i nazwisko właściciela:</span> <b>{breweryData.ceo}</b>
+                    </div>
                 )}
-                <p>
-                    Opis:
-                    <p className={styles.descriptionText}>
-                        {breweryData.description}
-                    </p>
-                </p>
+                
+                <div className={`${styles.infoCard} ${styles.employees}`}>
+                    <FaUserTie className={styles.icon} />
+                    <span>Ilość pracowników:</span> <b>{breweryData.no_employees}</b>
+                </div>
+
+                {breweryData.description !== '' &&
+                    <div className={styles.descriptionSection}>
+                        <h4>Opis:</h4>
+                        <p className={styles.descriptionText}>{breweryData.description}</p>
+                    </div>
+                }
             </div>
         </div>
     );
