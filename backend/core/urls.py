044ca9822574e868bc6377f55cf6c71aa1a85156
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2025-01-19 14:31:17 +0100

    order contract brewery details

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -34,6 +34,11 @@ urlpatterns = [
 
     path("orders/add/", views.OrderCreateView.as_view(), name="order_create"),
     path("orders/<int:order_id>/details/", views.OrderDetailView.as_view(), name="order_detail"),
+    path(
+        "orders/<int:order_id>/contract-brewery/details/",
+        views.OrderContractBreweryDetailView.as_view(),
+        name="order_contract_brewery_detail"
+    ),
     path("orders/<int:order_id>/accept/", views.OrderAcceptView.as_view(), name="order_accept"),
     path("orders/<int:order_id>/reject/", views.OrderRejectView.as_view(), name="order_reject"),
     path("orders/<int:order_id>/withdraw/", views.OrderWithdrawView.as_view(), name="order_withdraw"),
