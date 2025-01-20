1b01f9931260bf6b901da69c14a5a6d7c9a5efbc
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-20 01:54:40 +0100

    stats sketch

--- app/src/components/pages/intermediary/Statistics.jsx
+++ app/src/components/pages/intermediary/Statistics.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 import DashboardHeader from '../../modules/DashboardHeader.jsx';
 import IntermediarySidebar from '../../modules/intermediary/IntermediarySidebar.jsx';
 
-
 import styles from './Statistics.module.css';
 
 const Statistics = () => {
     return (
         <div>
             <DashboardHeader />
-            <div className={styles.appContainer}>
+            <div className={styles.container}>
                 <IntermediarySidebar />
-                <div className={styles.content}>
-                    
-                </div>
+                <div className={styles.content}></div>
             </div>
         </div>
     );
