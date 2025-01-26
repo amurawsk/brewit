a491b143b03a34dc9c5e5d46505202c24b8594af
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-26 15:12:59 +0100

    fixed displaying contract & commercial brewery name

--- app/src/components/modules/intermediary/IntermediaryDashboardContent.jsx
+++ app/src/components/modules/intermediary/IntermediaryDashboardContent.jsx
@@ -83,7 +83,11 @@ const IntermediaryDashboardContent = () => {
                                     <h2>Zlecenie #{order.id}</h2>
                                     <p>
                                         Zleceniobiorca:{' '}
-                                        <b>{order.contract_brewery_name}</b>
+                                        <b>{order.commercial_brewery.name}</b>
+                                    </p>
+                                    <p>
+                                        Zleceniodawca:{' '}
+                                        <b>{order.contract_brewery.name}</b>
                                     </p>
                                     <p>
                                         Utworzone dnia:{' '}
@@ -125,7 +129,11 @@ const IntermediaryDashboardContent = () => {
                                     <h2>Zlecenie #{order.id}</h2>
                                     <p>
                                         Zleceniobiorca:{' '}
-                                        <b>{order.contract_brewery_name}</b>
+                                        <b>{order.commercial_brewery.name}</b>
+                                    </p>
+                                    <p>
+                                        Zleceniodawca:{' '}
+                                        <b>{order.contract_brewery.name}</b>
                                     </p>
                                     <p>
                                         Utworzone dnia:{' '}
